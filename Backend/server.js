6b03ccc7766f445d7aad32737f6934cd8728b37f
IbrahimAnas <anas.jameel.dev@pk.see.biz>
2023-10-03 19:11:45 +0500

    commit __ test

--- Backend/server.js
+++ Backend/server.js
@@ -6,9 +6,18 @@ require('dotenv').config('.env');
 app.use(express.json())
 const PORT = process.env.PORT || 3030;
 
-connectDB();
 
-app.listen(PORT,()=>{
-    console.log(`Server is listening on port ${PORT}`)    
-});
+
+
+
+const start = async ()=>{
+    await connectDB();
+   try{
+       app.listen(PORT,()=>{
+            console.log(`Server is listening on port ${PORT}`)    
+        });
+   } catch(error){
+    console.error(`internal server ${error}`)
+   }
+}
            
